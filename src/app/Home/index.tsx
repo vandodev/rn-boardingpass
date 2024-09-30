11ab0ce03ad08f63f9ce2a861bdd111497c58362
vandodev <vandoaparecido@hotmail.com>
2024-09-30 14:07:00 -0300

    :airplane: :rocket: :airplane: Configuração base

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+import {styles } from './styles';
+
+export  function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Olá mundo</Text>     
+    </View>
+  );
+}
+
+
