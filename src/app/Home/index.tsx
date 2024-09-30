bf2a128732be535f6950eb681643b3a55879bb84
vandodev <vandoaparecido@hotmail.com>
2024-09-30 18:33:13 -0300

    :airplane: :rocket: :airplane: Criando header

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,16 @@
-import { Text, View } from 'react-native';
+import { Text, View, ImageBackground  } from 'react-native';
 import {styles } from './styles';
 
 export  function Home() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo</Text>     
+       <ImageBackground
+        style={styles.header}
+        source={require("@/assets/cover.png")}
+      >
+        <Text style={styles.title}>Cartão de embarque</Text>
+        <Text style={styles.subtitle}>Falta 45 dias para sua viagem</Text>
+      </ImageBackground>  
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -8,4 +8,20 @@ export const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: colors.black,
   },
+  header: {
+    width: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: colors.white,
+    fontSize: 24,
+    fontFamily: fontFamily.extrabold,
+  },
+  subtitle: {
+    color: colors.gray[300],
+    fontSize: 14,
+    fontFamily: fontFamily.medium,
+  },
 });
